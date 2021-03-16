952caba80759d58b6a7a6b3f43f4f5394f56f60d
JIUJU ZHOU <58928322+jiujuzhou@users.noreply.github.com>
2021-03-16 16:29:45 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,125 +1,76 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
+//step1, build the menu
+var lis;
+var ul1 = document.getElementById("navbar__list");
+window.onload =function() {
+    function fun(x){   
+        var liEle = document.createElement("li");
+        liEle.setAttribute = "oldcolor";
+        var aa = document.createElement("a");
+        aa.innerHTML="section"+String(x);
+        //aa.setAttribute("class", "nav_button");
+        liEle.appendChild(aa);
+        ul1.appendChild(liEle);
+    }
+    
+    for(i=0; i<4; i++){
+        fun(i+1);
+    }
 
-/**
- * Define Global Variables
- * 
-*/
+    
+ //step2, Scroll to section on link click   
+    lis = document.getElementsByTagName("li");
+    function scroll(section){
+        document.getElementById(section).scrollIntoView({block: "end", behavior: "smooth"});
+     }
 
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
-
- function scroll1(){
-    document.getElementById("section1").scrollIntoView({block: "end", behavior: "smooth"});
- }
- function scroll2(){
-    document.getElementById("section2").scrollIntoView({block: "end", behavior: "smooth"});
- }
- function scroll3(){
-    document.getElementById("section3").scrollIntoView({block: "end", behavior: "smooth"});
- }
- function scroll4(){
-    document.getElementById("section4").scrollIntoView({block: "end", behavior: "smooth"});
- }
-
-lis = document.getElementsByTagName("li");
-lis[0].addEventListener("click",scroll1,false);
-lis[1].addEventListener("click",scroll2,false);
-lis[2].addEventListener("click",scroll3,false);
-lis[3].addEventListener("click",scroll4,false);
-
-
-function change1(){
-    lis[0].style.backgroundColor='teal';
-}
-function change2(){
-    lis[1].style.backgroundColor='teal';
-}
-function change3(){
-    lis[2].style.backgroundColor='teal';
-}
-function change4(){
-    lis[3].style.backgroundColor='teal';
+    function addclick_forall(num){
+        for (var i=0; i<num; i++){
+            let section = "section"+String(i+1);
+            lis[i].addEventListener("click", function(e){
+                e.preventDefault();
+                scroll(section);
+             },false);
+        }
+    }
+    addclick_forall(4);
 }
 
-function change5(){
-    lis[0].style.backgroundColor='#02103E';
+
+ //step3, Set sections as active
+function getViewport(){
+    if (document.compatMode == "BackCompat"){
+        return {
+            width: document.body.clientWidth,
+            height: document.body.clientHeight
+        };
+    } else {
+        return {
+            width: document.documentElement.clientWidth,
+            height: document.documentElement.clientHeight
+        }; 
+    }
 }
-function change6(){
-    lis[1].style.backgroundColor='#02103E';
+
+//do the scroll function, and highlight navigation menu
+window.onscroll = function() {  
+    var in_viewport;
+    for (var i=0; i<4;i++){
+        var box = document.getElementById("section"+String(i+1));
+        var rect = box.getBoundingClientRect();
+        //console.log(rect);
+        viewport = getViewport();
+        //see if currection section in viewport or not.
+        if (rect.top<viewport.height-200 && rect.bottom>(viewport.height-window.innerHeight+200)) {
+            //console.log("in viewport");
+            lis[i].classList.add("nav_button");
+        }
+        else{
+            //console.log("not in viewport");
+            lis[i].classList.remove("nav_button");
+        }
+    }
+   
 }
-function change7(){
-    lis[2].style.backgroundColor='#02103E';
-}
-function change8(){
-    lis[3].style.backgroundColor='#02103E';
-}
-var sec1 = document.getElementById("section1");
-var sec2 = document.getElementById("section2");
-var sec3 = document.getElementById("section3");
-var sec4 = document.getElementById("section4");
-
-sec1.addEventListener("mouseover",change1, false);
-sec2.addEventListener("mouseover",change2, false);
-sec3.addEventListener("mouseover",change3, false);
-sec4.addEventListener("mouseover",change4, false);
-
-sec1.addEventListener("mouseout",change5, false);
-sec2.addEventListener("mouseout",change6, false);
-sec3.addEventListener("mouseout",change7, false);
-sec4.addEventListener("mouseout",change8, false);
-
-
-
-
-
 
 /*
 function doStuff() {
